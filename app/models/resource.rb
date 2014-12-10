cee67769aa70e3913af11dd3cf692adc94869758
Tanner Welsh <tannerwelsh@gmail.com>
2014-12-10 00:19:50 -0500

    Can search for Resources in particular category
    Adds two new methods: Resource#in_category and Resource#valid_category?,
    each of which takes a category string as input.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,15 @@
 class Resource < ActiveRecord::Base
   include PgSearch
 
-  pg_search_scope :search_in_readme, against: :readme
-end
+  CATEGORIES = %w{ exercises projects examples }
 
+  pg_search_scope :search_in_readme, against: :readme
+
+  def self.in_category(category)
+    self.where("manifest->>'category' = ?", category.to_s)
+  end
+
+  def self.valid_category?(category)
+    CATEGORIES.include? category.downcase
+  end
+end
