b74556b35256c3bdac52045d82584c73a35c9dc3
Tanner Welsh <tannerwelsh@gmail.com>
2014-12-10 00:59:42 -0500

    Override Resource#as_json to return just the manifest

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,6 +5,11 @@ class Resource < ActiveRecord::Base
 
   pg_search_scope :search_in_readme, against: :readme
 
+  # This is a hack. Better to use something like ActiveModel::Serializer
+  def as_json(options = {})
+    self.manifest
+  end
+
   def self.in_category(category)
     self.where("manifest->>'category' = ?", category.to_s)
   end
